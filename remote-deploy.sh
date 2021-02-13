2c67e57736c6c4008b2688c21de4146e8b2a2193
Jacob Errington <jerrington@drw.com>
2021-02-12 23:18:04 -0500

    bare minimum

---
+++ remote-deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -r build/ tsani@jerrington.me:/srv/http/labcoders
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Container, Header, Icon, Input} from "semantic-ui-react";
+
+interface AudioProps {
+    volume: number;
+}
+
+const Audio = ({volume}: AudioProps) => {
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    if (null !== audioRef.current && audioRef.current.volume !== volume) {
+        audioRef.current.volume = volume;
+    }
+
+    return (
+        <audio ref={audioRef} autoPlay>
+            <source src="http://radio.labcoders.club/audio"/>
+        </audio>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [volume, setVolume] = useState(0.0);
+
+    useEffect(() => {
+        const v = parseFloat(localStorage.getItem('volume') ?? '0');
+        setVolume(v);
+    }, []);
+
+    const doSetVolume = (v: number) => {
+        localStorage.setItem('volume', v.toString());
+        setVolume(v);
+    };
+
+    return (
+        <Container textAlign="center">
+            <Header as="h1" icon>
+                <Icon name="music"/>
+                Labcoders Radio
+            </Header>
+
+            <div>
+                <Input label="Volume" min="0" max="100" value={volume} onChange={(e) => doSetVolume(parseInt(e.target.value))} type="range"/>
+                <Audio volume={volume/100}/>
+            </div>
+        </Container>
+    );
 }
 
 export default App;
